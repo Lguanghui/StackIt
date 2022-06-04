2f02c9a72d656795b8d61b97377e5b57dc13cff0
liangguanghui <liangguanghui@keep.com>
2022-06-04 23:02:26 +0800

    Initial Commit

---
+++ StackIt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StackIt
+//
+//  Created by 梁光辉 on 2022/6/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
